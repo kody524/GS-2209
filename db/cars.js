880b7d4b8dedb889a86557ef6125c411d9f042e5
safirtrew <safirtrew@gmail.com>
2023-02-28 20:30:39 -0500

    cars.js updated

--- db/cars.js
+++ db/cars.js
@@ -1,17 +1,77 @@
-const client = require("./client")
+const client = require("./client");
 
-async function createCar({make,model,year,price,inventory,condition,engine,transmission,drivetrain,fuel,exteriorcolor,interiorcolor,description}){
-try{
-const{rows:[car]}= await client.query(`
+async function createCar({
+  make,
+  model,
+  year,
+  price,
+  inventory,
+  condition,
+  engine,
+  transmission,
+  drivetrain,
+  fuel,
+  exteriorcolor,
+  interiorcolor,
+  description,
+}) {
+  try {
+    const {
+      rows: [car],
+    } = await client.query(
+      `
 INSERT INTO cars(make,model,year,price,inventory,condition,engine,transmission,drivetrain,fuel,exteriorcolor,interiorcolor,description)
 VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)
 RETURNING *
-`,[make,model,year,price,inventory,condition,engine,transmission,drivetrain,fuel,exteriorcolor,interiorcolor,description])
-return car
-}catch(error){
-    console.log(error,"error creating car")
+`,
+      [
+        make,
+        model,
+        year,
+        price,
+        inventory,
+        condition,
+        engine,
+        transmission,
+        drivetrain,
+        fuel,
+        exteriorcolor,
+        interiorcolor,
+        description,
+      ]
+    );
+    return car;
+  } catch (error) {
+    console.log(error, "error creating car");
+  }
 }
-}
+
+async function getAllCars(){
+    try{
+        const{rows}= await client.query(`
+        SELECT * FROM cars;`)
+        return rows } catch(error){
+            throw error
+        }
+    }
+async function getCarsbyId(){try{
+    const{rows:[cars]}= await client.query(`
+  SELECT * FROM cars
+  WHERE id=${id};  
+    `)
+    
+    return cars
+  }catch(error){
+    throw error
+  }
+  
+  }
+async function updateCars ({})
+async function deleteCars ({})
 module.exports = {
-    createCar
-}
+  createCar,
+  getAllCars,
+  getCarsbyId,
+  updateCars,
+  deleteCars,
+};
