8353997b2e109a0c5c4670e9707c19f44d9f0f7a
kody524 <kodyrichardson@kodys-mbp.lan>
2023-03-01 20:31:48 -0600

    displaying something

--- src/Cars.js
+++ src/Cars.js
@@ -3,14 +3,27 @@ import { getAllCars } from "./allApiCalls";
 
 export function Cars(){
     const [cars,setCars]=useState([])
-
+console.log(cars)
 useEffect(()=>{
 getAllCars(setCars)
 },[])
 return(
-    cars.map((ele,idx)=>{
-        <p>{ele}</p>
+<>
+{
+    cars.map(ele=>{
+        return(<>
+            <p>make:{ele.make}</p>
+            <p>model:{ele.model}</p>
+            <p>year:{ele.year}</p>
+            <p>price:{ele.price}</p>
+            <p>inventory:{ele.inventory}</p>
+            <p>condition:{ele.condition}</p>
+            <p>engine:{ele.engine}</p>
+            </>
+        )
     })
+}
+</>
 )
 
 }
