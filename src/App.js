9fe0e34c539dd61a5ba4d09a6e9c9a0d97ab6a7f
kody524 <kodyrichardson@kodys-mbp.lan>
2023-03-08 19:03:55 -0600

    trying to merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import { Cars } from "./Cars";
+import { Cars } from "./Components/Cars";
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { SignIn } from "./Components/Login";
+import { SignUp } from "./Components/Register";
+import NavBar from './Components/NavBar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-import { SignIn } from "./Login";
-import { SignUp } from "./Register";
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#7D1E1E',
+    },
+    secondary: {
+      main: '#6B5656',
+    },
+    background: {
+      default: '#2A373F',
+      paper: '#BCAAA4',
+    },
+    text: {
+      primary: 'rgba(63,53,53,0.87)',
+      secondary: 'rgba(65,56,56,0.87)',
+      disabled: 'rgba(49,46,46,0.87)',
+    },
+  },
+});
+
 function App() {
   const [firstname, setFirstName] = useState('');
   const [lastname, setLastName] = useState('');
@@ -15,10 +38,33 @@ function App() {
   const [phone, setPhone] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
 console.log("first",firstname,lastname)
   return (
-    
-
+    <>
+      <SignUp
+        firstname={firstname}
+        setFirstName={setFirstName}
+        setLastName={setLastName}
+        lastname={lastname}
+        email={email}
+        setEmail={setEmail}
+        street={street}
+        setStreet={setStreet}
+        city={city}
+        setCity={setCity}
+        state={state}
+        setState={setState}
+        zip={zip}
+        setZip={setZip}
+        phone={phone}
+        setPhone={setPhone}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />
+    </>
   );
 }
 
