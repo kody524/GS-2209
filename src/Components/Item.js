2ac26e4c3090454c3c1351b21021cd34dbbd7d79
Julian Schieneman <jcatlanta@gmail.com>
2023-03-14 00:00:12 -0400

    edited carousel

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -3,8 +3,8 @@ import { Paper, Button } from '@mui/material'
 function Item ({item})
 {
     return (
-        <Paper>
-            <img src={item.image} alt={item.title} style={{width:"100%", height: "45vh"}} />
+        <Paper style={{backgroundColor:"red", display:"flex", justifyContent:"center"}}>
+            <img src={item.image} alt={item.title} style={{width:"auto", height: "45vh"}} />
           
            </Paper>
     )
