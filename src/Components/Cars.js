c6e6c601de399d238e41f8177ccac997e3118c16
kody524 <kodyrichardson@kodys-mbp.lan>
2023-03-15 17:56:43 -0500

    updated cars

--- src/Components/Cars.js
+++ src/Components/Cars.js
@@ -1,18 +1,44 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React, {useState,useEffect} from "react";
 import { getAllCars, getSingleCar } from "../allApiCalls";
-import {Card, Grid, Typography, Button, CardContent, Dialog, Modal} from "@mui/material";
+import {Card, Grid, Typography, Button, CardContent, Dialog, Modal, CardActionArea, ButtonGroup} from "@mui/material";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import NavBar from "./NavBar";
-import SingleCarModal from "./SingleCarModal";
-export function Cars({cars, setCars,loginSuccess}){
+import { useParams } from "react-router-dom";
+import { borderRadius, flexbox } from "@mui/system";
+import styles from './Cars.module.css'
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    borderRadius: "15px"
+  };
+  const style1 = {
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    borderRadius: "15px",
+    direction: "row"
+  };
+export function Cars({cars, setCars, car, setCar, loginSuccess}){
+    const {carId} = useParams()
 useEffect(() => {
-   
-    getAllCars(setCars)
+    getAllCars(setCars).then(cars);
     },[]);
-
-
-const [searchQuery, updateSearchQuery] = useState('')
+// useEffect(() => {
+//     getSingleCar(carId, setCar)
+//     }, [setCar, carId])
+    const id = localStorage.getItem("id")
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const [searchQuery, updateSearchQuery] = useState('')
 let carsToDisplay = cars;
 function carMatches(car, text) {
     const searchTerm = text.toLowerCase();
@@ -37,6 +63,35 @@ if(searchQuery.length > 0) {
 return(
 <>
 <NavBar loginSuccess={loginSuccess}></NavBar>
+<Modal
+    open={open}
+    onClose={()=>{handleClose()
+    }}
+  >
+    <Card sx={style}>
+      <Typography  sx={{ mt: 2 }}>
+            <h2>Vehicle Details</h2>
+                  <img src={cars.img} alt={cars.make}></img>
+                  <Typography>{cars.make}</Typography>
+                  <Typography>{cars.model}</Typography>
+                  <Typography>Year: {cars.description}</Typography>
+                  <Typography>Inventory: {cars.make}</Typography>
+                  <Typography>Condition: {cars.make}</Typography>
+                  <Typography>Engine: {cars.make}</Typography>
+                  <Typography>Transmission: {cars.make}</Typography>
+                  <Typography>Drivetrain: {cars.make}</Typography>
+                  <Typography>Fuel Type: {cars.make}</Typography>
+                  <Typography>Exterior Color: {cars.make}</Typography>
+                  <Typography>Interior Color: {cars.make}</Typography>
+                  <Typography>Description: {cars.make}</Typography>
+                  <Typography>Price: {cars.price}</Typography>
+                    <Button sx={{mr:2}} variant="contained" onClick={()=>{
+                    handleClose()
+                  }}>Return to All Vehicles</Button>
+                    <Button variant="contained">Add to Cart</Button>
+      </Typography>
+    </Card>
+  </Modal>
 <h1>Cars</h1>
 <h2>Search</h2>
 <input
@@ -45,24 +100,31 @@ return(
     value = {searchQuery}
     onChange = {(event) => {updateSearchQuery(event.target.value)}}
 />
-<Grid>
-{cars.map((car) => {
+<Grid  item container xs={6} className={styles.carContainer}>
+{cars.map((cars) => {
     return (<>
-        <NavBar loginSuccess={loginSuccess}/>
-        <Grid item xs={12} sm={6} md={4} lg={3} key={cars.vehicle_id}>
-            <Card>
-                <CardContent>
-                   
-                    <Typography variant="h5">{car.make}</Typography>
-                    <Typography variant="h6">{car.model}</Typography>
-                    <Typography variant="h6">{car.price}</Typography>
-                    <Typography variant="body1">{car.description}</Typography>
-                    {/* <Button variant="contained" onClick={<SingleCarModal carId={cars.id}/>}>Vehicle Single View</Button> */}
-                </CardContent>
+            <Card key={cars.id} sx={style1}>
+                        <CardContent>
+                            <img src={cars.img} alt={cars.make} style={{ alignitems:"center", maxWidth: '50%', maxheight: '50%' }}/>
+                        </CardContent>
+                        <CardContent>
+                            <Typography variant="h4">{cars.make}</Typography>
+                            <Typography variant="h5">{cars.model}</Typography>
+                            <Typography variant="h6">Price: ${cars.price}</Typography>
+                            {/* <Typography variant="body1" nowrap="false" width="30vw" >Description: {cars.description}</Typography> */}
+                            <ButtonGroup>
+                                <Button variant="contained" sx={{mr:2, ml:2, borderRadius:3}} onClick={()=>{
+                                handleOpen()
+                                getSingleCar(cars.id)}}
+                                >Vehicle Details</Button>
+                                <Button variant="contained"sx={{mr:2, borderRadius:3}}>
+                                Add To Cart
+                                <AddShoppingCartIcon/>
+                            </Button>
+                            </ButtonGroup>
+                        </CardContent>
             </Card>
-        </Grid>
    </> );
-})}
-</Grid></>)
+})}</Grid></>)
 }
 export default Cars;
