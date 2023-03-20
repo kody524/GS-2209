e7ddbd59a491144fb4d09712aff94670593a483e
Julian <jcatlanta@gmail.com>
2023-03-19 20:15:45 -0400

    updated footer and home page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,62 @@
-import NavBar from './NavBar';
-import Copyright from './Copyright';
+import React from 'react';
+import { Container, Box, Typography, Link } from '@mui/material';
+import { Facebook, Twitter, Instagram } from '@mui/icons-material';
 
+export default function Footer() {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+        backgroundColor: 'primary.main',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Container
+        maxWidth="lg"
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          height: '60px', 
+          padding: '0 24px',
+        }}
+      >
+        {/* Navigation Links */}
+        <Box>
+          <Link href="/" color="inherit" underline="none" mr={2}>
+            Home
+          </Link>
+          <Link href="/Cars" color="inherit" underline="none" mr={2}>
+            Cars
+          </Link>
+          <Link href="/Login" color="inherit" underline="none">
+            Login
+          </Link>
+        </Box>
 
+        {/* Social Media Links */}
+        <Box>
+          <Link href="#" color="inherit" underline="none" mr={1}>
+            <Facebook />
+          </Link>
+          <Link href="#" color="inherit" underline="none" mr={1}>
+            <Twitter />
+          </Link>
+          <Link href="#" color="inherit" underline="none">
+            <Instagram />
+          </Link>
+        </Box>
 
-const Footer = props => {
-    return(
-    <div>
-        <NavBar />
-        <div>
-            Footer
-        </div>
-        <Copyright />
-    </div>
-    )
-};
-
-export default Footer;
+        {/* Copyright */}
+        <Typography variant="body2" color="inherit" align="center">
+          &copy; {new Date().getFullYear()} Team 2 
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
