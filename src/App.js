ffd6d30e289cfbf4bc553ef7687d2113f6a32f18
Julian <jcatlanta@gmail.com>
2023-03-19 20:43:36 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Cars } from "./Components/Cars";
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SignIn } from "./Components/Login";
 import { SignUp } from "./Components/Register";
+import EditCar from "./Components/Editcar";
 import NavBar from './Components/NavBar';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Login } from "@mui/icons-material";
+import Home from "./Components/Home";
+import AdminEditCar from "./Components/Admin";
+import Admin from "./Components/Admin";
+import Checkout from "./Components/Checkout";
+import AddCar from "./Components/AddCar";
 
 const theme = createTheme({
   palette: {
@@ -39,33 +47,60 @@ function App() {
   const [phone, setPhone] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const[token,setToken]=useState('')
+  const[userId,setUserId]=useState(0)
+  const[cars,setCars]=useState([])
+  const[success,setSuccess]=useState(false)
+const[loginSuccess,setLoginSuccess]=useState(false)
+const[car,setCar]=useState([])
+const[carId,setCarId]=useState(0)
 
 
   return (
+   
     <>
-      {/* <SignUp
-        firstname={firstname}
-        setFirstName={setFirstName}
-        setLastName={setLastName}
-        lastname={lastname}
-        email={email}
-        setEmail={setEmail}
-        street={street}
-        setStreet={setStreet}
-        city={city}
-        setCity={setCity}
-        state={state}
-        setState={setState}
-        zip={zip}
-        setZip={setZip}
-        phone={phone}
-        setPhone={setPhone}
-        username={username}
-        setUsername={setUsername}
-        password={password}
-        setPassword={setPassword}
-      /> */}
-      <SignIn setUsername={setUsername} username={username} setPassword={setPassword} password={password}/>
+     <BrowserRouter>
+     <Routes>
+      <Route
+      path="/"
+      element={<Home loginSuccess={loginSuccess}/>}
+      >
+      </Route>
+      <Route
+      path="/login"
+      element={<SignIn username={username} setUsername={setUsername} password={password} setPassword={setPassword} setToken={setToken} setUserId={setUserId} token={token}  userId={userId} setLoginSuccess={setLoginSuccess} loginSuccess={loginSuccess} />}
+      >
+      </Route>
+      <Route
+      path="/register"
+      element={<SignUp firstname={firstname} setFirstName={setFirstName} lastname={lastname} setLastName={setLastName} email={email} setEmail={setEmail} street={street} setStreet={setStreet} zip={zip} setZip={setZip} phone={phone} setPhone={setPhone} success={success}
+      city={city} setCity={setCity} state={state} setState={setState} setSuccess={setSuccess} username={username} setUsername={setUsername} password={password} setPassword={setPassword} loginSuccess={loginSuccess}/>}
+      ></Route>
+      <Route 
+      path="/cars"
+      element={<Cars cars={cars} setCars={setCars} car={car} setCar={setCar} setCarId={setCarId} loginSuccess={loginSuccess}/>}
+      >
+      </Route>
+      <Route
+      path="/editcar"
+      element={<EditCar carId={carId} car={car} setCar={setCar}/>}
+      >
+      </Route>
+      <Route 
+      path="/admin"
+      element={<Admin/>}
+      >
+        
+      </Route>
+      <Route
+        path='/checkout'
+        element={<Checkout/>}
+        >
+        </Route>
+        <Route path='/addcar'element={<AddCar/>}>
+        </Route>
+     </Routes>
+     </BrowserRouter>
     </>
   );
 }
